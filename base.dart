19e434db19af6187efec7a9631b6c8b0349c2f37
Sunny <lesunhe@gmail.com>
2023-03-07 01:04:51 +0900

    data type & list

--- base.dart
+++ base.dart
@@ -1,42 +1,36 @@
 void main() {
-  // 관습적으로 함수, 메소드 내부 지역 변수 var 사용
-  var name = "sunny";
-  var number = 1;
-  var dynamicvar;
-  // var 선언과 동시에 초기화 안할 시 dynamic 타입으로 여러 자료형으로 변할 수 있음.
-  dynamicvar = 3;
-  dynamicvar = "hello";
-
-  if (dynamicvar is String) {
-    //이 안에서는 자료형 확정이므로 string 관련 함수를 사용가능
-    dynamicvar.substring(1);
-  }
-  // class 변수, property를 선언할 때는 타입지정
-  String nickname;
-  nickname = "sunny";
+  //자료형
+  //모두 클래스(객체) 구성임 -> 객체 지향 언어인 이유
+  //vscode로 보았을 때 자료형 위에 마우스 올리면 class임을 알 수 있음.
+  String name = "Sunny";
+  bool alive = true;
   int age = 25;
+  double money = 69.99;
+  // int ,double은 num을 상속받음 => num은 int ,double의 부모class
+  num x = 12;
+  x = 1.1;
+  // 변수.(변수 자료형에 맞는 댜양한 함수 존재)
 
-//null safety(null값이 에러 불러올 상황 방지)
-  String weather = "sunny";
-//weather = null; //-> null safety
-//null값일수도 string 일수도 있다는 것 명시
-  String? weathername = "sunny";
-  weathername = null;
-
-//final: 값 수정 불가능(상수)
-  final pie = 3.14;
-// pie = 1; => 에러
-
-// late: 초기값 설정없이 변수 선언가능
-// 주로, api로 데이터를 받아 나중에 데이터를 넣어줄 때 사용
-  late final double e;
-// 초기화하지 않고 print 등에 사용할 경우 값 넣기전에 접근하지 말라고 에러남.
-//print(e);
-  e = 2.718;
-
-//const : 값 수정 불가능(상수)
-//final vs const
-//final : 런타임에 값 할당 가능 ->api 할당 가능
-//const : 컴파일 타임에 값 할당 ->api 할당 불가능
-// 위의 pie나 e 등은 const, 그 외에도 max 값, min값도 활용 가능
+  //list : 객체들의 순서가 있는 컬렉션
+  //일반적으로 같은 자료형(dynamic 사용시 요소 자료형이 다를 수도 있는데 그럼 정적 검사가 불가능해서 type-sate가 보장x)
+  //리스트 끝에 ,로 마무리하면 줄마다 각 요소 1개씩으로 정렬됨
+  // collection if, collection for 지원
+  var giveMeFive = true;
+  var next = [6, 7, 8];
+  var numbers = [
+    1,
+    2,
+    3,
+    4,
+    //A : collection if (조건을 만족하는 경우에만 리스트에 요소를 추가)
+    if (giveMeFive) 5,
+    //B : collection for (리스트 내부에 for문을 사용하여 요소를 생성)
+    for (var i in next) i,
+  ];
+  //A와 동일한 기능
+  // if(giveMeFive)
+  // {
+  //   numbers.add(5);
+  // }
+  print(numbers);
 }
