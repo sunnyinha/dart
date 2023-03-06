acdcb45f2de4eab721db33d38e245b6492109296
Sunny <lesunhe@gmail.com>
2023-03-07 01:25:31 +0900

    map & set

--- base.dart
+++ base.dart
@@ -1,8 +1,15 @@
 void main() {
-  //문자 보간 (text에 변수를 추가 하는 법)
-  //$변수이름 사용
-  var name = "sunny";
-  var count = 2;
-  var greeting = "hello, I'm $name and I have ${count + 1} puppies";
-  print(greeting);
+  //map : key와 value 쌍으로 구성된 데이터 구조
+  var player = {
+    "name": "sunny",
+    "xp": 9.99,
+    "superpower": false,
+  };
+  //Set: 순서가 없는 고유한(유니크함) 요소 집합
+  //list와 같이 시퀀스한 데이터 구조(but set 요소는 list와 달리 유일하다는 점이 다름)
+  Set<int> numbers = {1, 2, 3, 4};
+  numbers.add(1);
+  numbers.add(1);
+  numbers.add(-1);
+  print(numbers);
 }
