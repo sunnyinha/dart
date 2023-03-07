2b7cca22ee9882c5aff0e2a5d1929eddce55e020
Sunny <lesunhe@gmail.com>
2023-03-07 13:19:30 +0900

    function start

--- base.dart
+++ base.dart
@@ -1,15 +1,22 @@
+//리턴 값 없는 함수
+//void 아무것도 return 하지 않음
+void ansHello() {
+  print("Nice to meet you too.");
+}
+
+//return 값이 있는 함수
+String sayHello(String name) {
+  return "Hello $name nice to meet you!";
+}
+
+//one line code 즉시 리턴 시
+//return, 중괄호 빼고 => 로도 표현 가능
+String sayGood() => "GOOD";
+num plus(num a, num b) => a + b;
+
 void main() {
-  //map : key와 value 쌍으로 구성된 데이터 구조
-  var player = {
-    "name": "sunny",
-    "xp": 9.99,
-    "superpower": false,
-  };
-  //Set: 순서가 없는 고유한(유니크함) 요소 집합
-  //list와 같이 시퀀스한 데이터 구조(but set 요소는 list와 달리 유일하다는 점이 다름)
-  Set<int> numbers = {1, 2, 3, 4};
-  numbers.add(1);
-  numbers.add(1);
-  numbers.add(-1);
-  print(numbers);
+  print(sayHello("sunny"));
+  ansHello();
+  print(sayGood());
+  print(plus(1, 2));
 }
