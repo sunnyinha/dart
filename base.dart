1dd0948d6dcced2a7721ae693b081d5112b9ce7c
Sunny <lesunhe@gmail.com>
2023-03-07 23:53:09 +0900

    class start

--- base.dart
+++ base.dart
@@ -1,15 +1,17 @@
-//typedef 함수 타입의 별칭 지정 키워드
-// typedef ListOfInts = List<int>;
+// class
+//데이터와 해당 데이터를 조작하는 함수(메소드)를 하나의 단위로 묶어서 추상화된 개념
+class Player {
+  final String name = "Sunny";
+  int xp = 1500;
 
-// ListOfInts reverseListOfNumbers(ListOfInts list) {
-//   var reversed = list.reversed;
-//   return reversed.toList();
-// }
-
-typedef UserInfo = Map<String, String>;
-String sayHi(UserInfo userInfo) => "Hi ${userInfo["name"]}";
+  void sayHello() {
+    print("Hi my name is $name");
+  }
+}
 
 void main() {
-  // print(reverseListOfNumbers([1, 2, 3]));
-  print(sayHi({"name": "sunny"}));
+  var player = Player();
+  print(player.name);
+  // player.name = "lalala"; -> player name이 final로 지정되어 있어서 변경 불가
+  player.sayHello();
 }
