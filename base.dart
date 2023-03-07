9a0c9bfd22973503666676d70d9fd51ae7baf1e6
Sunny <lesunhe@gmail.com>
2023-03-07 19:15:10 +0900

    operators : ternary operator, ??, ??=

--- base.dart
+++ base.dart
@@ -1,15 +1,21 @@
-//Optional Positional Parameters
-//함수 매개 변수 중 선택적으로 전달할 수 있는 위치 매개 변수
-//선택적 매개 변수는 대괄호([])를 사용하여 정의(null도 가능하도록 ? 붙여줌)
-void printGreetings(String name, [String? greeting]) {
-  if (greeting != null) {
-    print('$greeting, $name!');
-  } else {
-    print('Hello, $name!');
-  }
-}
+//연산자들
+//ex)대문자 교체 (null 값일 경우 ANON 리턴)
+
+//3항 연산자 + fat arrow
+// String capitalizeName (String? name) =>
+//   name != null ? name.toUpperCase() : 'ANON';
+
+// ??(qq 연산자) + fat arrow 사용
+// String capitalizeName(String? name) => name?.toUpperCase() ?? "ANON";
 
 void main() {
-  printGreetings('Sunny');
-  printGreetings('Sunny', 'Good morning');
+  // capitalizeName("sunny");
+  // capitalizeName(null);
+
+  // ??=  : 값이 null이면 오른쪽 값 할당
+  String? name;
+  name ??= "Sunny";
+  name = null;
+  name ??= "wow";
+  print(name);
 }
