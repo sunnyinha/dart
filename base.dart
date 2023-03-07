d048f82c344873da25855e3d328fb9f20001ef7a
Sunny <lesunhe@gmail.com>
2023-03-08 00:14:47 +0900

    constructor

--- base.dart
+++ base.dart
@@ -1,8 +1,22 @@
 // class
 //데이터와 해당 데이터를 조작하는 함수(메소드)를 하나의 단위로 묶어서 추상화된 개념
 class Player {
-  final String name = "Sunny";
-  int xp = 1500;
+  //late : 변수에 나중에 값 받아올거임
+  // late final String name;
+  // late int xp;
+
+  // 생성자(constructor)
+  // Player(String name, int xp) {
+  //   this.name = name;
+  //   this.xp = xp;
+  // }
+
+  //위의 코드 간단하게
+  final String name;
+  int xp;
+
+// 생성자(constructor)
+  Player(this.name, this.xp);
 
   void sayHello() {
     print("Hi my name is $name");
@@ -10,8 +24,8 @@ class Player {
 }
 
 void main() {
-  var player = Player();
-  print(player.name);
-  // player.name = "lalala"; -> player name이 final로 지정되어 있어서 변경 불가
+  var player = new Player("Sunny", 100);
   player.sayHello();
+  var player2 = Player("Cloudy", 200);
+  player2.sayHello();
 }
