8ae873f712cef2ebfe4984d2ff41fc8a4cc70fbd
Sunny <lesunhe@gmail.com>
2023-03-09 01:46:27 +0900

    Mixin

--- base.dart
+++ base.dart
@@ -1,30 +1,19 @@
-//Inheritance
-//부모 클래스의 특징과 기능을 자식 클래스에게 물려주는 것
-//extends 키워드를 사용하여 상속 관계를 선언
+class Strong {
+  final double strenghtLevel = 1500.99;
+}
 
-class Human {
-  String name;
-  Human({required this.name});
-  void sayHello() {
-    print("Hi my name is $name");
+class QuickRunner {
+  void runQuick() {
+    print("ruuuun!!!");
   }
 }
 
-enum Team { blue, red }
-
-class Player extends Human {
-  final Team team;
-//super
-//자식 클래스에서 부모 클래스의 멤버에 접근할 때 사용하는 키워드
-  Player({required this.team, required String name}) : super(name: name);
-
-  @override
-  void sayHello() {
-    super.sayHello();
-    print('and I play for ${team}');
-  }
-}
+//Mixin
+//클래스에서 특정 기능을 구현한 다른 클래스의 코드를 가져와서 사용할 수 있게 해줌
+//with 키워드를 사용하여 정의
+class Player with Strong, QuickRunner {}
 
 void main() {
-  var player = Player(team: Team.red, name: "SUNNY");
+  Player player = new Player();
+  player.runQuick();
 }
