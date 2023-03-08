4acc11b1f0481b7ded3fc59a7ef96a02f6dead4a
Sunny <lesunhe@gmail.com>
2023-03-09 00:19:20 +0900

    cascade operation

--- base.dart
+++ base.dart
@@ -1,40 +1,28 @@
 // class
 //데이터와 해당 데이터를 조작하는 함수(메소드)를 하나의 단위로 묶어서 추상화된 개념
 class Player {
-  final String name;
+  String name;
   int xp;
   String team;
 
-  Player.fromJson(Map<String, dynamic> playerjson)
-      : name = playerjson["name"],
-        xp = playerjson["xp"],
-        team = playerjson["team"];
+  Player({
+    required this.name,
+    required this.xp,
+    required this.team,
+  });
+
   void sayHello() {
     print("Hi my name is $name");
   }
 }
 
 void main() {
-  var apiData = [
-    {
-      "name": "Sunny",
-      "team": "red",
-      "xp": 0,
-    },
-    {
-      "name": " Cloudy",
-      "team": "red",
-      "xp": 0,
-    },
-    {
-      "name": "Rainy",
-      "team": "red",
-      "xp": 0,
-    },
-  ];
-
-  apiData.forEach((playerJson) {
-    var player = Player.fromJson(playerJson);
-    player.sayHello();
-  });
+  var sunny = Player(name: "sunny", xp: 1200, team: "red");
+  //cascade operation
+  //하나의 객체에 대해 여러 변수, 메소드를 연속적으로 호출가능
+  var getsunny = sunny
+    ..name = "GOD"
+    ..xp = 1200000
+    ..team = "blue"
+    ..sayHello();
 }
